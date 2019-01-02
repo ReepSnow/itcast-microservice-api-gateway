644d164327ee5682fce4e0261a30cd31a171f566
wangpengtao <1020194587@qq.com>
2019-01-02 23:25:27 +0800

    Initial commit

---
+++ src/main/java/cn/itcast/microservice/gateway/ApiGatewayApplication.java
@@ -0,0 +1,20 @@
+package cn.itcast.microservice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
+
